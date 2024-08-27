202f514d48da661a7e9a9353e18beeceb026fa47
Adarsh Tiwari <tiwariadarsh9910@gmail.com>
2024-08-27 11:28:21 +0530

    new update

--- index.js
+++ index.js
@@ -105,5 +105,5 @@ io.on('connection',(socket) => {
 })
 
 server.listen(PORT,() => {
-    console.log("Server running on port",PORT);
+    console.log(`Server running on port: http://localhost:${PORT}`);
 })
